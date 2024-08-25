bfbff19e23c5c286689bf972cdc6c121215d780d
tristan <tristanbidault@gmail.com>
2024-08-25 10:39:50 +0200

    Initial commit

---
+++ my-vue-app/src/components/ReqComp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const message = ref('');
+
+onMounted(() => {
+  axios.get('http://localhost:3000/')
+    .then(response => {
+      message.value = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching message:', error);
+    });
+});
+</script>
